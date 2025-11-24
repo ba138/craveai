a284ad1b4580b90da3166798d57b96761203e96a
Basit ali <basitalyshah51214@gmail.com>
2025-11-23 23:56:39 -0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/craveai/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.craveai
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/controllers/app_colors.dart
@@ -0,0 +1,14 @@
+class AppColors {
+  static const int primary = 0xFFFFFFFF;
+  static const int primaryVariant = 0xFF3700B3;
+  static const int secondary = 0xFF03DAC6;
+  static const int secondaryVariant = 0xFF018786;
+  static const int background = 0x000000;
+  static const int surface = 0xFFFFFFFF;
+  static const int error = 0xFFB00020;
+  static const int onPrimary = 0xFFFFFFFF;
+  static const int onSecondary = 0xFF000000;
+  static const int onBackground = 0xFF000000;
+  static const int onSurface = 0xFF000000;
+  static const int onError = 0xFFFFFFFF;
+}
