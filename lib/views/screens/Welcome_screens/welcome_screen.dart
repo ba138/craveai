df4606a40f10b691b2f0c6532563e13a1a316800
Basit ali <basitalyshah51214@gmail.com>
2025-11-24 00:51:10 -0800

    Update color definitions, add WelcomeScreen components, and implement MyButton widget

--- lib/views/screens/Welcome_screens/welcome_screen.dart
+++ lib/views/screens/Welcome_screens/welcome_screen.dart
@@ -1,5 +1,8 @@
+import 'package:craveai/controllers/app_colors.dart';
 import 'package:craveai/generated/assets.dart';
 import 'package:craveai/views/widgets/common_image_view.dart';
+import 'package:craveai/views/widgets/my_button.dart';
+import 'package:craveai/views/widgets/my_text.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatefulWidget {
@@ -21,6 +24,41 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
               height: 300,
               width: double.maxFinite,
               fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 20),
+            CommonImageView(
+              imagePath: Assets.logo,
+              height: 80,
+
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 8),
+
+            MyText(
+              text: 'Your AI Girlfriend. Always here.\n Always yours',
+              textAlign: TextAlign.center,
+              size: 13,
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 40.0,
+                right: 40.0,
+                bottom: 20,
+                top: 20,
+              ),
+              child: MyButton(
+                onTap: () {},
+                buttonText: "Create an account",
+                radius: 12,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MyText(text: "Already have an account?", size: 13),
+                MyText(text: " Sign In", color: AppColors.secondary, size: 14),
+              ],
             ),
           ],
         ),
---
+++ lib/views/screens/auth_screens/lgoin_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LgoinScreen extends StatefulWidget {
+  const LgoinScreen({super.key});
+
+  @override
+  State<LgoinScreen> createState() => _LgoinScreenState();
+}
+
+class _LgoinScreenState extends State<LgoinScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
